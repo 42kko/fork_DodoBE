170cceecbbe2dc51919e66967fb5602629b23d73
42kko <kko@student.42seoul.kr>
2024-11-07 19:15:10 +0900

    1

--- src/main/java/dododocs/dododocs/auth/domain/repository/MemberRepository.java
+++
@@ -1,9 +0,0 @@
-package dododocs.dododocs.auth.domain.repository;
-
-import dododocs.dododocs.member.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-    boolean existsByEmail(String email);
-    Member findByEmail(String email);
-}
--- src/main/java/dododocs/dododocs/auth/exception/NoExistMemberException.java
+++
@@ -1,7 +0,0 @@
-package dododocs.dododocs.auth.exception;
-
-public class NoExistMemberException extends RuntimeException {
-    public NoExistMemberException(String message) {
-        super(message);
-    }
-}
