f9ede9fee94b6549cda462d6fa29b9e0b833a1f7
42kko_syncbot <runner@fv-az1391-445.32quejtkpmmezbyick4efsqy3f.bx.internal.cloudapp.net>
2024-12-10 10:54:37 +0000

    Sync from source repository

--- src/main/java/dododocs/dododocs/chatbot/application/ChatbotService.java
+++ src/main/java/dododocs/dododocs/chatbot/application/ChatbotService.java
@@ -1,7 +1,51 @@
 package dododocs.dododocs.chatbot.application;
 
+import dododocs.dododocs.analyze.domain.RepoAnalyze;
+import dododocs.dododocs.analyze.domain.repository.RepoAnalyzeRepository;
+import dododocs.dododocs.analyze.exception.NoExistRepoAnalyzeException;
+import dododocs.dododocs.chatbot.domain.ChatLog;
+import dododocs.dododocs.chatbot.domain.repository.ChatLogRepository;
+import dododocs.dododocs.chatbot.dto.ExternalQuestToChatbotRequest;
+import dododocs.dododocs.chatbot.dto.ExternalQuestToChatbotResponse;
+import dododocs.dododocs.chatbot.dto.FindChatLogResponses;
+import dododocs.dododocs.chatbot.infrastructure.ExternalChatbotClient;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.awt.print.Pageable;
+import java.util.List;
+
+@RequiredArgsConstructor
 @Service
 public class ChatbotService {
+    private final ExternalChatbotClient externalChatbotClient;
+    private final RepoAnalyzeRepository repoAnalyzeRepository;
+    private final ChatLogRepository chatLogRepository;
+
+    public ExternalQuestToChatbotResponse questionToChatbotAndSaveLogs(final long registeredRepoId, final String question) {
+        final RepoAnalyze repoAnalyze = repoAnalyzeRepository.findById(registeredRepoId)
+                .orElseThrow(() -> new NoExistRepoAnalyzeException("레포지토리 정보가 존재하지 않습니다."));
+
+        final List<ChatLog> chatLogs = chatLogRepository.findTop3ByRepoAnalyzeOrderBySequenceDesc(repoAnalyze);
+
+        final List<ExternalQuestToChatbotRequest.RecentChatLog> recentChatLogs = chatLogs.stream()
+                .map(chatLog -> new ExternalQuestToChatbotRequest.RecentChatLog(chatLog.getQuestion(), chatLog.getAnswer()))
+                .toList();
+
+        final ExternalQuestToChatbotRequest questToChatbotRequest = new ExternalQuestToChatbotRequest(
+                repoAnalyze.getRepoUrl(),
+                question,
+                recentChatLogs,
+                false
+        );
+        return externalChatbotClient.questToChatbot(questToChatbotRequest);
+    }
+
+    public FindChatLogResponses findChatbotHistory(final long registeredRepoId, final Pageable pageable) {
+        final RepoAnalyze repoAnalyze = repoAnalyzeRepository.findById(registeredRepoId)
+                .orElseThrow(() -> new NoExistRepoAnalyzeException("레포 정보 없습니다."));
+
+        return new FindChatLogResponses(chatLogRepository.findByRepoAnalyze(repoAnalyze, pageable));
+    }
 }
+
--- src/main/java/dododocs/dododocs/chatbot/presentation/ChatbotController.java
+++ src/main/java/dododocs/dododocs/chatbot/presentation/ChatbotController.java
@@ -3,22 +3,32 @@ package dododocs.dododocs.chatbot.presentation;
 import dododocs.dododocs.auth.dto.Accessor;
 import dododocs.dododocs.auth.presentation.authentication.Authentication;
 import dododocs.dododocs.chatbot.application.ChatbotService;
+import dododocs.dododocs.chatbot.dto.FindChatLogResponses;
+import dododocs.dododocs.chatbot.dto.QuestToChatbotRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.awt.print.Pageable;
 
 @RequiredArgsConstructor
 @RestController("/api/chatbot")
 public class ChatbotController {
     private final ChatbotService chatbotService;
 
-    @PostMapping("/question/save")
-    public ResponseEntity<Void> questionToChatbotAndSaveLogs() {
+    @PostMapping("/question/save/{registeredRepoId}")
+    public ResponseEntity<Void> questionToChatbotAndSaveLogs(@Authentication final Accessor accessor,
+                                                             @PathVariable final Long registeredRepoId,
+                                                             @RequestBody final QuestToChatbotRequest questToChatbotRequest) {
+        chatbotService.questionToChatbotAndSaveLogs(registeredRepoId, questToChatbotRequest.getQuestion());
         return ResponseEntity.noContent().build();
     }
 
-    @GetMapping("/logs/{repoId}")
-    public ResponseEntity<Void> findChatbotTalkLogs(@Authentication final Accessor accessor, @PathVariable final long repoId) {
-        return ResponseEntity.noContent().build();
+    @GetMapping("/logs/{registeredRepoId}")
+    public ResponseEntity<FindChatLogResponses> findChatbotHistory(@Authentication final Accessor accessor,
+                                                                   @PathVariable final Long registeredRepoId,
+                                                                   @PageableDefault(size = 30) final Pageable pageable) {
+        return ResponseEntity.ok(chatbotService.findChatbotHistory(registeredRepoId, pageable));
     }
 }
